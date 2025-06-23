c29f0b7093bc3f36f79213db57cf60c9a19b3143
ThiagoSilva <thiagoantonio2108@gmail.com>
2025-06-23 11:55:26 -0300

    Finalizado aula webapi

---
+++ webapi/src/main/java/com/jkalango/webapi/AloController.java
@@ -0,0 +1,18 @@
+package com.jkalango.webapi;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController
+@RequestMapping("/alo")
+public class AloController {
+
+      //requisição e resposta (RESQUEST E RESPONSE)
+      @GetMapping
+      public String aloMundo(){
+          return "Alô Mundo - Jkalango";
+      }
+
+}
---
+++ webapi/src/main/java/com/jkalango/webapi/FaseJogoController.java
@@ -0,0 +1,15 @@
+package com.jkalango.webapi;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/fases")
+public class FaseJogoController {
+    
+    @GetMapping
+    public int mostrarQtdFases(){
+        return 7;
+    }
+}
