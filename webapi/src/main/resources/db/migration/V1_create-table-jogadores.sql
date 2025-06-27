1f6930b9b6d19c26b45b3b395c4ac6a8ad0bad17
ThiagoSilva <thiagoantonio2108@gmail.com>
2025-06-27 11:43:39 -0300

    Concluindo Jkalango

--- webapi/src/main/resources/db/migration/V1_create-table-jogadores.sql
+++ webapi/src/main/resources/db/migration/V1_create-table-jogadores.sql
@@ -1,7 +1,7 @@
 CREATE TABLE jogador(
     id BIGINT NOT NULL auto_increment,
     nome VARCHAR(100) NOT NULL,
-    nickName VARCHAR(100) NOT NULL,
+    nick_name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
     telefone VARCHAR(100) NOT NULL,
     senha VARCHAR(50) NOT NULL,
